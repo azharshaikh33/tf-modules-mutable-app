d1a95095d2627bbec539868e99d62d8b0582ee53
Azhar <azharshaikh33@gmail.com>
2024-05-30 17:43:33 +0400

    Auto Push

--- data.tf
+++ data.tf
@@ -18,7 +18,7 @@ data "terraform_remote_state" "alb" {
     }
 }
 
-data "aws_ami" "my_ami" {
+data "aws_ami" "My_ami" {
   most_recent = true
   name_regex = "Lab-Ami-With-Ansible_Installed"
   owners = ["self"]
