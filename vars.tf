f2f5af9f1ad2d9df72e3c22b4216e6955e86f31c
Azhar <azharshaikh33@gmail.com>
2024-06-09 17:16:34 +0400

    Auto Push

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "ENV" {}
 variable "INSTANCE_TYPE" {}
 variable "SPOT_INSTANCE_COUNT" {}
-variable "OD_INSTANCE_TYPE" {}
+variable "OD_INSTANCE_COUNT" {}
 variable "COMPONENT" {}
 variable "APP_PORT" {}
